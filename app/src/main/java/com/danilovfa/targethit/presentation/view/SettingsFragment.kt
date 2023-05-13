7791f0b4656c30413b0f3bcd0d6bce7a3c023822
Fyodor Danilov <danilovfa@protonmail.com>
2023-05-13 14:49:33 +0300

    Add settings fragment

---
+++ app/src/main/java/com/danilovfa/targethit/presentation/view/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.danilovfa.targethit.presentation.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.danilovfa.targethit.databinding.FragmentSettingsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@AndroidEntryPoint
+class SettingsFragment : Fragment() {
+
+    private val binding: FragmentSettingsBinding by viewBinding(CreateMethod.INFLATE)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
