53cdf328a9197a9e33dcadd785bfaa366a65f5a0
Fyodor Danilov <danilovfa@protonmail.com>
2023-05-16 16:23:21 +0300

    Add presentation's coordinate mapper

--- app/src/main/java/com/danilovfa/targethit/presentation/model/CoordinateMapper.kt
+++ app/src/main/java/com/danilovfa/targethit/presentation/model/CoordinateMapper.kt
@@ -1,4 +1,26 @@
 package com.danilovfa.targethit.presentation.model
 
-class CoordinateMapper {
+import com.danilovfa.targethit.domain.model.Coordinate
+import com.danilovfa.targethit.utils.Constants.Companion.DOMAIN_GAME_FIELD_HEIGHT
+import com.danilovfa.targethit.utils.Constants.Companion.DOMAIN_GAME_FIELD_WIDTH
+
+class CoordinateMapper(
+    private val viewWidth: Int,
+    private val viewHeight: Int,
+) {
+    fun fromView(coordinate: Coordinate): Coordinate {
+        return Coordinate(
+            x = coordinate.x * DOMAIN_GAME_FIELD_WIDTH / viewWidth,
+            y = coordinate.y * DOMAIN_GAME_FIELD_HEIGHT / viewHeight,
+            t = coordinate.t
+        )
+    }
+
+    fun fromData(coordinate: Coordinate): Coordinate {
+        return Coordinate(
+            x = coordinate.x * viewWidth / DOMAIN_GAME_FIELD_WIDTH,
+            y = coordinate.y * viewHeight / DOMAIN_GAME_FIELD_HEIGHT,
+            t = coordinate.t
+        )
+    }
 }
