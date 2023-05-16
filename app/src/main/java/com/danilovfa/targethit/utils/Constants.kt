984770d0bee52a9ff4cbc1b8b3b6b6a86f61eac3
Fyodor Danilov <danilovfa@protonmail.com>
2023-05-16 16:23:44 +0300

    Add constants

--- app/src/main/java/com/danilovfa/targethit/utils/Constants.kt
+++ app/src/main/java/com/danilovfa/targethit/utils/Constants.kt
@@ -3,6 +3,16 @@ package com.danilovfa.targethit.utils
 class Constants {
     companion object {
         const val LEADERBOARD_SIZE = 10
-        const val FPS = 50
+        const val FPS = 120
+        // Can only be dividable by 10. Less than or equal to 1000
+        // i.e. 1, 10, 100, 1000
+        const val STOPWATCH_UPDATE_TIME = 10
+        const val TARGET_SIZE_DP = 100
+        const val CROSSHAIR_SIZE_DP = 50
+        const val CROSSHAIR_SPEED = 5
+        const val GAME_FIELD_PADDING_DP = 5
+        const val SCORE_UPDATE_VALUE = 5
+        const val DOMAIN_GAME_FIELD_WIDTH = 600
+        const val DOMAIN_GAME_FIELD_HEIGHT = 1200
     }
 }
